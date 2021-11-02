d668034163425ebbec926664a64fa318348b4446
Praveen <praveensbj8@gmail.com>
2021-11-02 11:16:18 +0530

    index js file

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-let sortButton = document.getElementsByClassName('sort-btn')[0];
-let shuffleButton = document.getElementsByClassName('shuffle-btn')[0];
+const sortButton = document.getElementsByClassName('sort-btn')[0];
+const shuffleButton = document.getElementsByClassName('shuffle-btn')[0];
 const colorArray = ['#6f98a8', '#2b8ead', '#2f454e', '#2b8ead', '#2f454e', '#bfbfbf', '#bfbfbf', '#6e97a7', '#2f454e'];
 const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-sortButton.addEventListener('click', function (event) {
+// Sort Elements
+const sort = (event) => {
     let elementList = new Array(...document.getElementsByClassName('now'));
     elementList = elementList.sort();
     document.querySelector('.num-section').innerHTML = '';
@@ -13,9 +14,10 @@ sortButton.addEventListener('click', function (event) {
         document.querySelector('.num-section').appendChild(elementList[i]);
     }
     styleContent();
-});
+}
 
-shuffleButton.addEventListener('click', function (event) {
+// Shuffle elements
+const shuffle = (event) => {
     let elementList = new Array(...document.getElementsByClassName('now'));
     elementList = elementList.sort(() => Math.random() - 0.5);
     for (let i = 0, len = elementList.length; i < len; i++) {
@@ -23,9 +25,10 @@ shuffleButton.addEventListener('click', function (event) {
         let detatchedItem = parent.removeChild(elementList[i]);
         parent.appendChild(detatchedItem);
     }
-});
+}
 
-function styleContent() {
+// Styles the cards based on card number
+const styleContent = () => {
     let elementList = new Array(...document.getElementsByClassName('now'));
     document.querySelector('.num-section').innerHTML = '';
     for (let i = 0, len = elementList.length; i < len; i++) {
@@ -36,9 +39,10 @@ function styleContent() {
     }
 }
 
-function loadContent() {
+// Loads Elements in the browser
+const loadContent = () => {
     let mainDiv = document.querySelector('.num-section');
-    let eArray = [];
+    const eArray = [];
     numbers.forEach((number, i) => {
         const ele = `<div class="now">${number}</div>`
         eArray.push(ele)
@@ -46,3 +50,7 @@ function loadContent() {
     mainDiv.innerHTML = eArray.join('\n');
     styleContent()
 }
+
+// Event Listeners added to the Shuffle and Sort buttons
+shuffleButton.addEventListener('click', shuffle);
+sortButton.addEventListener('click',  sort);
